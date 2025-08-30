8c6f4c60c432a057d8ec60d8e474c996e9cb736a
Dev Null <devnull@example.com>
2025-08-30 04:05:01 -0300

    chore: remove dist from index

--- dist/config/ensureAdmin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ensureAdmin = ensureAdmin;
-function ensureAdmin(req, res, next) {
-    if (req.userRole !== "ADMIN") {
-        return res.status(403).json({ message: "Acesso negado." });
-    }
-    return next();
-}
