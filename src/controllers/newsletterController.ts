70da24126967917567099cc0cfc7a91b0febaa8d
Dev Null <devnull@example.com>
2026-01-31 01:02:58 -0300

    fix(backend): fix newsletter controller import

--- src/controllers/newsletterController.ts
+++ src/controllers/newsletterController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { prisma } from '../index';
+import { prisma } from '../config/prisma';
 import { z } from 'zod';
 
 export class NewsletterController {
