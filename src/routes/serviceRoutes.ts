c3a86db9462ceabe704122cd63270f7497ff96dd
Alyson Jean <alyson-jean@hotmail.com>
2026-02-05 17:37:17 -0300

    fix: Add processUpload middleware to service routes for Cloudinary upload

--- src/routes/serviceRoutes.ts
+++ src/routes/serviceRoutes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { ServiceController } from '../controllers/serviceController';
-import { uploadSingle } from '../middlewares/upload';
+import { uploadSingle, processUpload } from '../middlewares/upload';
 
 const router = Router();
 const controller = new ServiceController();
 
 router.get('/', controller.list);
 router.get('/:id', controller.get);
-router.post('/', uploadSingle('image'), controller.create);
-router.put('/:id', uploadSingle('image'), controller.update);
+router.post('/', uploadSingle('image'), processUpload, controller.create);
+router.put('/:id', uploadSingle('image'), processUpload, controller.update);
 router.delete('/:id', controller.delete);
 
 export default router;
